db74f1bbdb57ef9a8548dc05cc8600d9f3f37c6c
Joseph Dante Gallegos <JDGallegos@student.fullsail.edu>
2026-07-21 18:02:44 -0600

    Fix JWT expiration environment variable syntax error

--- backend-api/app/auth.py
+++ backend-api/app/auth.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(
     os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "60")
-
+)
 # Password hashing setup
 pwd_context = CryptContext(
     schemes=["bcrypt"],
